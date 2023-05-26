27c992bb864b0e62799dadfcfd0f24d602c870ab
SebnemHilal <sebnemhilal98@outlook.com>
2023-05-26 17:25:39 +0300

    creating task table

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $table = 'tasks';
+
+    protected $filltable = [
+        'name',
+        'is_completed',
+    ];
 }
