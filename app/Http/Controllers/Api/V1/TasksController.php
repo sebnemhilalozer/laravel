58ff85060ad9718f33eb015ce58d91e35f50f4a0
SebnemHilal <sebnemhilal98@outlook.com>
2023-06-01 08:46:57 +0300

    added validators

--- app/Http/Controllers/Api/V1/TasksController.php
+++ app/Http/Controllers/Api/V1/TasksController.php
@@ -4,35 +4,82 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Task;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
-use App\Http\Resources\TaskResource;
+use Illuminate\Support\Facades\Validator;
+
 
 
 class TasksController extends Controller
 {
     public function index()
     {
-        return TaskResource::collection(Task::all());
+        $tasks = TaskResource::collection(Task::all());
+        return $tasks;
+
+        if ($tasks->count() > 0) {
+
+            return response()->json([
+                'status' => 200,
+                'tasks' => $tasks,
+            ], 200);
+        } else {
+
+            return response()->json([
+                'status' => 404,
+                'tasks' => 'No records founds about tasks!',
+            ], 404);
+        }
     }
 
     public function store(StoreTaskRequest $request)
     {
         $task = Task::create($request->validated());
-
-        return TaskResource::make($task);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:211',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->getMessageBag()
+            ], 422);
+        } else {
+            return TaskResource::make($task);
+        }
     }
 
-    public function show(Task $task)
+    public function show($id)
     {
-        return TaskResource::make($task);
+        $task = Task::find($id);
+        if($task){
+            return response()->json([
+                'status'=> 200,
+                'task'=> $task
+            ],200);
+        }else{
+            return response()->json([
+                'status'=> 200,
+                'message'=> "No such task found!"
+            ],404);
+        }
+         
     }
 
     public function update(UpdateTaskRequest $request, Task $task)
     {
         $task->update($request->validated());
-
-        return TaskResource::make($task);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:211',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->getMessageBag()
+            ], 422);
+        } else {
+            return TaskResource::make($task);
+        }
     }
 
     public function destroy(Task $task)
--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,3 @@ Route::prefix('v1')->group(function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
